96117295bbddf6463afcb36a26056e9b3f30dbe8
Lucas Gehlen <lucasgehlen@Lucass-MacBook-Pro.local>
2018-12-05 12:56:01 +0100

    Added activity interrupts to main.py

--- pytrack/main.py
+++ pytrack/main.py
@@ -10,6 +10,7 @@ from machine import RTC
 from machine import SD
 from L76GNSS import L76GNSS
 from pytrack import Pytrack
+from LIS2HH12 import LIS2HH12
 
 time.sleep(2)
 gc.enable()
@@ -26,11 +27,47 @@ print('Adjusted from UTC to EST timezone', utime.localtime(), '\n')
 py = Pytrack()
 l76 = L76GNSS(py, timeout=30)
 
+# display the reset reason code and the sleep remaining in seconds
+# possible values of wakeup reason are:
+# WAKE_REASON_ACCELEROMETER = 1
+# WAKE_REASON_PUSH_BUTTON = 2
+# WAKE_REASON_TIMER = 4
+# WAKE_REASON_INT_PIN = 8
+time.sleep(2)
+print("Wakeup reason: " + str(py.get_wake_reason()) + "; Aproximate sleep remaining: " + str(py.get_sleep_remaining()) + " sec")
+time.sleep(0.5)
+
+# enable wakeup source from INT pin
+py.setup_int_pin_wake_up(False)
+
+# enable activity and also inactivity interrupts, using the default callback handler
+py.setup_int_wake_up(True, True)
+
+acc = LIS2HH12()
+# enable the activity/inactivity interrupts
+# set the accelereation threshold to 2000mG (2G) and the min duration to 200ms
+acc.enable_activity_interrupt(1500, 160)
+
+# check if we were awaken due to activity
+if acc.activity():
+    pycom.rgbled(0xFF0000)
+    coord = l76.coordinates()
+    #f.write("{} - {}\n".format(coord, rtc.now()))
+    print("{} - {} - {}".format(coord, rtc.now(), gc.mem_free()))
+    print("ACTIVITY!")
+else:
+    pycom.rgbled(0x00FF00)  # timer wake-up
+time.sleep(0.1)
+
+# go to sleep for 5 minutes maximum if no accelerometer interrupt happens
+py.setup_sleep(3600)
+py.go_to_sleep()
+
 # sd = SD()
 # os.mount(sd, '/sd')
 # f = open('/sd/gps-record.txt', 'w')
 
-while (True):
-    coord = l76.coordinates()
+# while (True):
+#    coord = l76.coordinates()
     #f.write("{} - {}\n".format(coord, rtc.now()))
-    print("{} - {} - {}".format(coord, rtc.now(), gc.mem_free()))
+#    print("{} - {} - {}".format(coord, rtc.now(), gc.mem_free()))
